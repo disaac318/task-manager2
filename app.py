0953ccd822b22bc8be72a8b0bd1fcd9f343c018e
Dean Isaac <shuttermaven@yahoo.ca>
2025-11-16 01:26:24 +0000

    add color to h1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World ... again!"
+    return "<H1 style='color:maroon'>Welcome to Task Manager</H1>"
 
 
 if __name__ == "__main__":
