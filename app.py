c57ba57d7c1a9580b622ea7747e585f45bfc2a0e
Dean Isaac <shuttermaven@yahoo.ca>
2025-11-16 01:23:55 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,35 +1,17 @@
 import os
-
-from flask import (
-    Flask,
-    flash,
-    render_template,
-    redirect,
-    request,
-    session,
-    url_for,
-)
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-
-
+from flask import Flask
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SECRET_KEY")
-
-mongo = PyMongo(app)
-
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:maroon'>Welcome to the Ultimate Task Manager</h1>"
+    return "Hello World ... again!"
 
- 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
